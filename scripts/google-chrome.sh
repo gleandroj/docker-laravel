9bf1851b623116ddc54e57d39b29bb16aea0e613
Gabriel L <gabrielleandrojunior@live.com>
2018-09-28 22:05:34 -0300

    Fix google script

--- scripts/google-chrome.sh
+++ scripts/google-chrome.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 ## Instala Google Chrome
 ########################
 
-apt-get install libxss1 fonts-liberation libappindicator3 xdg-utils libappindicator1 libindicator7 -y
+apt-get install libxss1 fonts-liberation libappindicator3-1 xdg-utils libappindicator1 libindicator7 -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome*.deb
 apt-get install -f -y
