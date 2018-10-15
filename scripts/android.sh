06045f389df4b9141071eebfdc4c51d0d5a78441
Gabriel L <gabrielleandrojunior@live.com>
2018-10-15 16:00:56 -0300

    Android NDK

--- scripts/android.sh
+++ scripts/android.sh
@@ -10,27 +10,8 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Instalando Android SDK..."
 
 #apt-get install -y android-sdk
-
 export GRADLE_VERSION=4.10
-cd /opt
-wget -q https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip
-unzip gradle*.zip
-mv gradle-${GRADLE_VERSION} gradle
-rm gradle*.zip
-
-# download and install Kotlin compiler
-# https://github.com/JetBrains/kotlin/releases/latest
-export KOTLIN_VERSION=1.2.61
-wget -q https://github.com/JetBrains/kotlin/releases/download/v${KOTLIN_VERSION}/kotlin-compiler-${KOTLIN_VERSION}.zip
-unzip *kotlin*.zip
-rm *kotlin*.zip
-
 export ANDROID_SDK_VERSION=4333796
-mkdir -p /opt/android-sdk
-cd /opt/android-sdk
-wget -q https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_VERSION}.zip
-unzip *tools*linux*.zip
-rm *tools*linux*.zip
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export GRADLE_HOME=/opt/gradle
@@ -43,6 +24,18 @@ export ANDROID_NDK_VERSION=r16b
 export ANDROID_NDK_URL=http://dl.google.com/android/repository/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.zip
 export ANDROID_NDK_HOME=${SDK_HOME}/android-ndk-${ANDROID_NDK_VERSION}
 export PATH=${ANDROID_NDK_HOME}:${ANDROID_HOME}/cmake/bin:$PATH
+
+cd /opt
+wget -q https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip
+unzip gradle*.zip
+mv gradle-${GRADLE_VERSION} gradle
+rm gradle*.zip
+
+mkdir -p /opt/android-sdk
+cd /opt/android-sdk
+wget -q https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_VERSION}.zip
+unzip *tools*linux*.zip
+rm *tools*linux*.zip
 
 curl -L "${ANDROID_NDK_URL}" -o android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.zip  \
   && unzip android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.zip -d ${SDK_HOME}  \
@@ -60,5 +53,3 @@ chmod u+x ${ANDROID_HOME}/ -R
 chmod u+x ${ANDROID_NDK_HOME}/ -R
 chmod u+x ${ANDROID_HOME}/cmake/bin/ -R
 chmod u+x ${SDK_HOME}/bin/android-wait-for-emulator
-
-#sdkmanager update sdk --no-ui
