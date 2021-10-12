aa60a34b79d9195fe050eb7cc8671d7105009341
Gabriel Siqueira <gabriel.southsystem@terceiros.zenvia.com>
2021-10-11 22:17:13 -0300

    chore: build

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -4,5 +4,11 @@ set -e
 echo -e "Running PHP-FPM Daemon..."
 php-fpm -D
 
+RETRIES=30
+until pg_isready -h $DB_HOST > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server: $DB_HOST, $((RETRIES--)) remaining attempts..."
+  sleep 1
+done
+
 echo -e "Running Nginx..."
 /usr/sbin/nginx -g "daemon off;"
