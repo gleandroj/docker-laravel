d55bd7063cf46a2ac79ca9185084a00785b9f8f0
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-04-15 13:02:16 -0300

    Update ngnix.sh

--- scripts/ngnix.sh
+++ scripts/ngnix.sh
@@ -11,12 +11,14 @@ export LANG=C.UTF-8
 
 echo "Instalando o Nginx..."
 
+#apt-add-repository ppa:nginx/stable -r -y
 apt-add-repository ppa:ondrej/php -y
-apt-add-repository ppa:nginx/stable -y
 apt-add-repository ppa:ondrej/apache2 -y
 
-apt-get update
-apt-get install -y nginx php7.1-fpm
+apt-get update -q
+apt-get install -y nginx \
+                   php7.4-fpm \
+                   nginx-extras
 
 service nginx reload
 service nginx restart
