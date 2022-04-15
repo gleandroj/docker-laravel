841fa2bbd5de4f6da6ff9374523fa9c7b7796b63
Gabriel Siqueira <gabriel.southsystem@terceiros.zenvia.com>
2022-04-15 13:16:14 -0300

    fix: php version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,32 +12,32 @@ source /etc/environment
 echo "Configurando o PHP..."
 
 # Altera configuracoes do PHP-CLI
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.1/cli/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.1/cli/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.1/cli/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.1/cli/php.ini
-sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.1/cli/php.ini
-sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.1/cli/php.ini
-sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.1/cli/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.4/cli/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/cli/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.4/cli/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.4/cli/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.4/cli/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.4/cli/php.ini
+sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.4/cli/php.ini
 
 # Altera as configuracoes do PHP-FPM
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.1/fpm/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.1/fpm/php.ini
-sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.1/fpm/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.1/fpm/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.1/fpm/php.ini
-sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.1/fpm/php.ini
-sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.1/fpm/php.ini
-sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.1/fpm/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.4/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.4/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.4/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.4/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.4/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.4/fpm/php.ini
+sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.4/fpm/php.ini
 
 # Configuracoes da fila do FPM
-sed -i "s/^user = www-data/user = ${DEPLOY_USER}/" /etc/php/7.1/fpm/pool.d/www.conf
-sed -i "s/^group = www-data/group = ${DEPLOY_USER}/" /etc/php/7.1/fpm/pool.d/www.conf
-sed -i "s/;listen\.owner.*/listen.owner = ${DEPLOY_USER}/" /etc/php/7.1/fpm/pool.d/www.conf
-sed -i "s/;listen\.group.*/listen.group = ${DEPLOY_USER}/" /etc/php/7.1/fpm/pool.d/www.conf
-sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.1/fpm/pool.d/www.conf
-sed -i "s/;request_terminate_timeout.*/request_terminate_timeout = 60/" /etc/php/7.1/fpm/pool.d/www.conf
-sed -i "s/;clear_env = yes/clear_env = no/" /etc/php/7.1/fpm/pool.d/www.conf
+sed -i "s/^user = www-data/user = ${DEPLOY_USER}/" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/^group = www-data/group = ${DEPLOY_USER}/" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/;listen\.owner.*/listen.owner = ${DEPLOY_USER}/" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/;listen\.group.*/listen.group = ${DEPLOY_USER}/" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/;request_terminate_timeout.*/request_terminate_timeout = 60/" /etc/php/7.4/fpm/pool.d/www.conf
+sed -i "s/;clear_env = yes/clear_env = no/" /etc/php/7.4/fpm/pool.d/www.conf
 
 # Ajusta as permicoes do diretorio de sessoes
 chmod 733 /var/lib/php/sessions
