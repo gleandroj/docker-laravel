29aaf54ba6bcb4fcbd2e64ded6e2041f3e967115
Gabriel Siqueira <gabrielleandrojunior@live.com>
2022-04-15 12:39:31 -0300

    Update php.sh

--- scripts/php.sh
+++ scripts/php.sh
@@ -4,39 +4,39 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive 
 
 ############################################
-## Instalar PHP 7.1
+## Instalar PHP 7.4
 ############################################
 
 add-apt-repository ppa:ondrej/php
 
 export LANG=C.UTF-8
 
-echo "Instalando o PHP 7.1 ..."
+echo "Instalando o PHP 7.4 ..."
+#php7.4-memcached \
 
 apt-get install -y  \
   libpcre3 \
-  php7.1-cli \
-  php7.1-dev \
-  php7.1-pgsql \
-  php7.1-sqlite3 \
-  php7.1-gd \
-  php7.1-curl \
-  php7.1-imap \
-  php7.1-mysql \
-  php7.1-imagick \
-  php7.1-mbstring \
-  php7.1-xml \
-  php7.1-json \
-  php7.1-zip \
-  php7.1-bcmath \
-  php7.1-soap \
-  php7.1-intl \
-  php7.1-readline \
-  php7.1-mcrypt \
-  php7.1-dba \
-  php7.1-opcache \
-  php7.1-xdebug \
-  php7.1-gmp \
+  php7.4-cli \
+  php7.4-dev \
+  php7.4-pgsql \
+  php7.4-sqlite3 \
+  php7.4-gd \
+  php7.4-curl \
+  php7.4-imap \
+  php7.4-mysql \
+  php7.4-imagick \
+  php7.4-mbstring \
+  php7.4-xml \
+  php7.4-json \
+  php7.4-zip \
+  php7.4-bcmath \
+  php7.4-soap \
+  php7.4-intl \
+  php7.4-readline \
+  php7.4-mcrypt \
+  php7.4-dba \
+  php7.4-opcache \
+  php7.4-xdebug \
   postgresql-client
 
 # Instala o Composer
