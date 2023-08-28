7d8487cb3c0d467e387e826452084527586a8cef
Gabriel Siqueira <gabrielleandrojunior@live.com>
2023-08-28 09:51:34 -0300

    Update android.sh

--- scripts/android.sh
+++ scripts/android.sh
@@ -19,20 +19,25 @@ export PATH=${GRADLE_HOME}/bin:$PATH
 # android sdk|build-tools|image
 export ANDROID_TARGET_SDK="android-28" \
     ANDROID_BUILD_TOOLS="27.0.3" \
-    ANDROID_SDK_TOOLS="3859397" \
+    ANDROID_SDK_TOOLS="10406996" \
     ANDROID_IMAGES="sys-img-armeabi-v7a-android-28,sys-img-armeabi-v7a-android-28"   
 
-export ANDROID_SDK_URL=https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_TOOLS}.zip
+# https://developer.android.com/studio#command-line-tools-only
+export ANDROID_SDK_URL=https://dl.google.com/android/repository/commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
+
+mkdir -p ${SDK_HOME}/android-sdk/cmdline-tools
 
 curl -sSL "${ANDROID_SDK_URL}" -o android-sdk-linux.zip \
-    && unzip android-sdk-linux.zip -d ${SDK_HOME}/android-sdk \
+    && unzip android-sdk-linux.zip -d ${SDK_HOME}/android-sdk/cmdline-tools \
   && rm -rf android-sdk-linux.zip
-  
+
+mv ${SDK_HOME}/android-sdk/cmdline-tools/cmdline-tools ${SDK_HOME}/android-sdk/cmdline-tools/tools 
+
 # Set ANDROID_HOME
 
 export ANDROID_HOME=${SDK_HOME}/android-sdk
 
-export PATH=${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:$PATH
+export PATH=${ANDROID_HOME}/cmdline-tools/tools/bin:$PATH
 
 # licenses
 mkdir $ANDROID_HOME/licenses
@@ -42,12 +47,12 @@ echo 24333f8a63b6825ea9c5514f83c2829b004d1fee >> $ANDROID_HOME/licenses/android-
 echo 84831b9409646a918e30573bab4c9c91346d8abd > $ANDROID_HOME/licenses/android-sdk-preview-license
 
 # Update and install using sdkmanager 
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "tools" "platform-tools"
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "build-tools;${ANDROID_BUILD_TOOLS}"
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "platforms;${ANDROID_TARGET_SDK}"
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "extras;android;m2repository" "extras;google;google_play_services" "extras;google;m2repository"
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.2"
-echo yes | $ANDROID_HOME/tools/bin/sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout-solver;1.0.2"
+echo yes | sdkmanager "tools" "platform-tools"
+echo yes | sdkmanager "build-tools;${ANDROID_BUILD_TOOLS}"
+echo yes | sdkmanager "platforms;${ANDROID_TARGET_SDK}"
+echo yes | sdkmanager "extras;android;m2repository" "extras;google;google_play_services" "extras;google;m2repository"
+echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.2"
+echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout-solver;1.0.2"
 
 # android ndk
 
